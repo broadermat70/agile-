1d6eae60fdc507228de73f3bc4056041b47b04cd
metaknighttu <54918445+metaknighttu@users.noreply.github.com>
2019-10-02 14:57:53 -0700

    Add files via upload
    The project name is still BubbleHelper, however this is now the base program from which further development will be built on.

---
+++ mainwindow.cpp
@@ -0,0 +1,39 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "guarantee.h"
+#include "contact.h"
+#include "environments.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_guaranteePolicyButton_clicked()
+{
+    guarantee guarantee;
+    guarantee.setModal(true);
+    guarantee.exec();
+}
+
+void MainWindow::on_contactInfoButton_clicked()
+{
+    contact contact;
+    contact.setModal(true);
+    contact.exec();
+}
+
+void MainWindow::on_supportedEnvironmentsButton_clicked()
+{
+    environments env;
+    env.setModal(true);
+    env.exec();
+}
---
+++ mainwindow.h
@@ -0,0 +1,29 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QStackedWidget>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_guaranteePolicyButton_clicked();
+
+    void on_contactInfoButton_clicked();
+
+    void on_supportedEnvironmentsButton_clicked();
+
+private:
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
