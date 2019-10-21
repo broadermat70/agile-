f7ea196207fe63aa880096fca0eae61abd6ac51a
Aziiz0 <54380763+Aziiz0@users.noreply.github.com>
2019-10-21 14:53:18 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,5 +7,14 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+    
+    int count = 0;
+    userType* allUsers = readUsers(count);
+    for(int i = 0; i < count; i++)
+    {
+        allUsers[i].setHasLoggedIn(false);
+    }
+    backupUsers(allUsers, count);
+    
     return a.exec();
 }
